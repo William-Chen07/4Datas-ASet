a77c8a9311d70a684ecb0a038f949dec157bd036
William-Chen07 <william0714chen@gmail.com>
2026-03-28 12:32:08 -0500

    Filtered top 10 count

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import pandas,numpy,matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read CSV
+dfLap = pd.read_csv("LapTimes.csv")
+dfRace = pd.read_csv("RaceResults.csv")
+top_ten = dfRace[dfRace["Position"] <= 10]
+top_ten_sorted = top_ten.sort_values(by="TeamName")
+print(top_ten_sorted[["TeamName","Position","FullName","Location"]].head(10))
+print(top_ten_sorted.groupby("TeamName").size().reset_index(name="Top 10 Count"))
